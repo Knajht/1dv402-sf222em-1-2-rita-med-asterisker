13f273cace1318b76a2540b0b3693f042b26072f
Sebastian Fernstedt <sf222em@student.lnu.se>
2014-09-25 09:58:53 +0200

    Basic code written, Level A finished.

--- 1dv402.S1.L02A/1dv402.S1.L02A/Program.cs
+++ 1dv402.S1.L02A/1dv402.S1.L02A/Program.cs
@@ -10,6 +10,32 @@ namespace _1dv402.S1.L02A
     {
         static void Main(string[] args)
         {
+            for (int j = 0; j < 25; j++)
+            {
+                switch (j % 3)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                }
+
+                if(j % 2 == 1)
+                {
+                Console.Write(" ");
+                }
+
+                for (int i = 0; i < 39; i++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
