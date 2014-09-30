3ac63055b18dd2f8ff489cb6e167b2d4ff7587d6
Sebastian Fernstedt <sf222em@student.lnu.se>
2014-09-30 13:57:59 +0200

    Started level B

---
+++ 1dv402.S1.L02B/1dv402.S1.L02B/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1dv402.S1.L02A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < 25; j++)
+            {
+                switch (j % 3)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                }
+
+                if(j % 2 == 1)
+                {
+                Console.Write(" ");
+                }
+
+                for (int i = 0; i < 39; i++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
